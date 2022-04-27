8c4a5c11f3908ff548ae02ae20d37d3bc7183232
chang <2622310479@qq.com>
2022-04-27 17:08:52 +0800

    chore: 更新 vite 开发配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> npm i @types/node -D
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
+/**
+ * 配置文档：https://cn.vitejs.dev/config/
+ */
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // 设置 `@` 指向 `src` 目录
+    },
+  },
+  base: './', // 设置打包路径
+  server: {
+    host: '0.0.0.0',
+    port: 8080, // 设置服务启动端口号
+    open: true, // 设置服务启动时是否自动打开浏览器
+    cors: true, // 允许跨域
+
+    // 设置代理，根据项目实际情况配置
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://xxx.xxx.xxx.xxx:8000',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace('/^\/api/', '/')
+    //   }
+    // }
+  },
 })
